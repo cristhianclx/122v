1bfb05abf9fa2a9c0ac6fad2ba08114182602cad
Cristhian <cristhianclx@gmail.com>
2025-01-14 22:46:08 -0500

    09

--- 09/main.py
+++ 09/main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from datetime import datetime, timedelta
+from flask import Flask, render_template, request, redirect, url_for
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from flask_socketio import SocketIO
@@ -72,11 +73,33 @@ def index():
     return render_template("index.html", items=items)
 
 
+@app.route("/rooms/create", methods=["GET", "POST"])
+def rooms_create():
+    if request.method == "GET":
+        return render_template("rooms-create.html")
+    if request.method == "POST":
+        data = request.form
+        item = Room(**data)
+        db.session.add(item)
+        db.session.commit()
+        return redirect(url_for('rooms_by_id', id=item.id))
+
+
 @app.route("/rooms/<id>")
 def rooms_by_id(id):
     room = Room.query.get_or_404(id)
     items = Message.query.filter_by(room = room).all()
-    return render_template("messages.html", room=room, items=items)
+    date_one_day_before = datetime.now() - timedelta(days = 1)
+    data = []
+    for item in items:
+        is_one_day_before = True
+        if date_one_day_before > item.created_at:
+            is_one_day_before = False
+        data.append({
+            "item": item,
+            "is_one_day_before": is_one_day_before, 
+        })
+    return render_template("messages.html", room=room, items=data, date_one_day_before=date_one_day_before)
 
 
 @socketio.on("ws-messages")
@@ -87,12 +110,3 @@ def handle_ws_message(data):
     raw = message_schema.dump(item)
     channel_id = "ws-messages-responses-{}".format(item.room.id)
     socketio.emit(channel_id, raw)
-
-
-# LABORATORIO
-# - create room, /rooms/create/
-# - hacer visible el created_at, pero para los mensajes mayores a un dia de generados
-#   poner hace mas de un dia
-#      user 1: 122v (2024-01-01 05:00:00)
-#      ....
-#      user 1: 122v (hace mas de un dia)
